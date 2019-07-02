e4eba854217faaf2419e377946637ad6b144ce03
root <root@localhost.localdomain>
2019-07-02 12:03:45 +0530

    initial commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,8 @@
-import React from 'react';
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
